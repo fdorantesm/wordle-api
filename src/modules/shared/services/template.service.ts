913a8dd3ab0251f45a86b542f420dd59754cf187
Fernando Dorantes <fdorantesm@gmail.com>
2022-12-15 18:58:43 -0600

    Remove unuseful shared services

--- src/modules/shared/services/template.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import * as pug from 'pug';
-
-@Injectable()
-export class TemplateService {
-  public render(template: string, data?: { [key: string]: any }): string {
-    return pug.render(template, data);
-  }
-}
--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,40 +1,13 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
-import { EmailService } from './services/email.service';
-import { FileService } from './services/file.service';
-import { S3Service } from './services/s3.service';
-import { ShortIdService } from './services/short-id.service';
-import { SlugService } from './services/slug.service';
 import { emailConfigLoader } from './../../core/infrastructure/config/loaders/email.loader';
-import { TemplateService } from './services/template.service';
-import { Titlelizer } from './services/titlelizer.service';
-import { UrlService } from './services/url.service';
+import { ShortIdService } from './services/short-id.service';
 import { DateService } from './services/date.service';
 
 @Module({
   imports: [ConfigModule.forFeature(emailConfigLoader)],
-  providers: [
-    S3Service,
-    SlugService,
-    ShortIdService,
-    FileService,
-    EmailService,
-    TemplateService,
-    Titlelizer,
-    UrlService,
-    DateService,
-  ],
-  exports: [
-    S3Service,
-    SlugService,
-    ShortIdService,
-    FileService,
-    EmailService,
-    TemplateService,
-    Titlelizer,
-    UrlService,
-    DateService,
-  ],
+  providers: [ShortIdService, DateService],
+  exports: [ShortIdService, DateService],
 })
 export class SharedModule {}
