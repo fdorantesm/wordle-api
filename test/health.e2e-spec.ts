9faea9e40fed0bd5a7b4fd7e9dbc951ab1a0168f
Fernando Dorantes Monsiváis <fdorantesm@gmail.com>
2023-01-03 03:54:49 -0600

    Health e2e test improvement

--- test/health.e2e-spec.ts
+++ test/health.e2e-spec.ts
@@ -1,23 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
-import { AppModule } from 'src/modules/app/app.module';
+import { HealthModule } from 'src/modules/health/health.module';
+import { HttpExceptionFilter } from 'src/core/infrastructure/filters/exception.filter';
+import { TransformInterceptor } from 'src/core/infrastructure/interceptors/transform.interceptor';
 
 describe('HealthController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [HealthModule],
+      providers: [
+        {
+          provide: APP_FILTER,
+          useClass: HttpExceptionFilter,
+        },
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: TransformInterceptor,
+        },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('GET /health', () => {
-    return request(app.getHttpServer())
+  it('GET /health', async () => {
+    await request(app.getHttpServer())
       .get('/health')
       .expect(HttpStatus.NO_CONTENT);
   });
